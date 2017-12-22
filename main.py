beac1352bf037c4e4547b07e1e741cda92757b64
Sergio Palechor <davidpalechor95@hotmail.com>
2017-12-21 22:44:43 -0500

    Update logger

--- main.py
+++ main.py
@@ -12,24 +12,13 @@ def send_message_demon(aodv_obj):
     aodv_obj.send("State Change!")
 
 def main():
+    # logging.basicConfig(filename="logs/aodv.log", 
+    #                     level=logging.DEBUG,
+    #                     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    logging.basicConfig(level=logging.DEBUG,
+                        format="[%(asctime)s - %(name)s] - %(levelname)s - %(message)s")
 
     logger = logging.getLogger("main")
-    logger.setLevel(logging.DEBUG)
-
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    # create file handler which logs even debug messages
-    fh = logging.FileHandler('spam.log')
-    fh.setLevel(logging.DEBUG)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
-
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.ERROR)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
 
     logger.info("Main triggered!")
 
