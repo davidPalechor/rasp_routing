da5bbe90587cab0769c3759a2b5c35322d8f6457
Sergio Palechor <davidpalechor95@hotmail.com>
2018-01-11 21:37:58 -0500

    Name refactoring

--- find_neighbours.py
+++ find_neighbours.py
@@ -5,10 +5,10 @@ from socket import *
 import routing as rt
 import logging
 
-class NeighborDiscovery(th.Thread):
+class NodeDiscovery(th.Thread):
     def __init__(self):
         th.Thread.__init__(self)
-        self.neighbors = []
+        self.nodes = []
         self.ip_address = gip.get_lan_ip()
         self.logger = logging.getLogger(__name__)
 
@@ -18,27 +18,27 @@ class NeighborDiscovery(th.Thread):
         s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
         s.sendto(msg, ('<broadcast>', 1200))
 
-    def add_neighbor(self, neighbor):
-        self.neighbors.append(neighbor)
+    def add_node(self, node):
+        self.nodes.append(node)
 
-    def send_neighbors_to_aodv(self):
-        self.logger.debug("Sending to AODV module '%s'" % str(self.neighbors))
+    def send_nodes_to_aodv(self):
+        self.logger.debug("Sending to AODV module '%s'" % str(self.nodes))
         s = socket(AF_INET, SOCK_DGRAM)
-        s.sendto(str(self.neighbors), (self.ip_address, 1212))
+        s.sendto(str(self.nodes), (self.ip_address, 1212))
 
-    def notify_neighbors(self):
-        th.Timer(60, self.notify_neighbors).start()
-        self.logger.debug("Notifying neighbors")
-        self.send_neighbors_to_aodv()
+    def notify_nodes(self):
+        th.Timer(60, self.notify_nodes).start()
+        self.logger.debug("Notifying nodes")
+        self.send_nodes_to_aodv()
 
-    def resend_neighbors(self,neighbors):
-        new_neighbor = 0
-        for addr in neighbors:
-            if addr not in self.neighbors and addr != self.ip_address:
-                self.add_neighbor(addr)
-                new_neighbor += 1
-        self.logger.debug("My Neighbors are: %s"%self.neighbors)
-        return new_neighbor
+    def resend_nodes(self,nodes):
+        new_node = 0
+        for addr in nodes:
+            if addr not in self.nodes and addr != self.ip_address:
+                self.add_node(addr)
+                new_node += 1
+        self.logger.debug("My Nodes are: %s"%self.nodes)
+        return new_node
 
     def listener(self):
         s = socket(AF_INET, SOCK_DGRAM)
@@ -46,18 +46,18 @@ class NeighborDiscovery(th.Thread):
         # SOCKET BINDING
         s.bind(('', 1200))
         while True:
-            (packet, neighbor) = s.recvfrom(1024)
+            (packet, node) = s.recvfrom(1024)
 
-            if neighbor[0] != self.ip_address:
+            if node[0] != self.ip_address:
                 if packet == "First message!":
-                    self.add_neighbor(neighbor[0])
-                    self.broadcast(str(self.neighbors))
+                    self.add_node(node[0])
+                    self.broadcast(str(self.nodes))
 
-                elif self.resend_neighbors(eval(packet) + [neighbor[0]]) > 0:
-                    self.broadcast(str(self.neighbors))
+                elif self.resend_nodes(eval(packet) + [node[0]]) > 0:
+                    self.broadcast(str(self.nodes))
 
     def run(self):
         self.logger.info("Listener ON %s" % self.ip_address)
         self.broadcast("First message!")
-        self.notify_neighbors()
+        self.notify_nodes()
         self.listener()
